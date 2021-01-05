28a8839b28eb008220e5a5f415941c9111563c44
rcarbal <rcarbaleq2@gmail.com>
2021-01-05 12:12:47 -0800

    Add env var

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const NAME = process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`YOUR NAME IS: ${NAME}!!!`);
     await sleep(5000);
   }
 }
